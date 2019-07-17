d2035220f7f1b01e6df2dbb1f489f648b2e3ef86
dynamic-router-3131 <jtboss47@gmail.com>
2019-07-17 00:16:24 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,12 +4,12 @@ class Dog
     @this_dogs_name = dogs_name 
   end
   
-  # def name 
-  #   @this_dogs_name
-  # end
+  def name 
+    @this_dogs_name
+  end
   
 end
 
-# lassie = Dog.new 
-# lassie.name = "Lassie"
-# puts lassie.name
+lassie = Dog.new 
+lassie.name = "Lassie"
+puts lassie.name
